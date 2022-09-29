3dc4e17e0f04428aee542a89ea821c27832b800f
Rauolli <rauol@t-online.de>
2022-09-29 17:43:00 +0200

    added info route and controller

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -12,7 +12,7 @@ const getSinglePokemon = async (req, res) => {
     const paramsID = req.params.id;
     try{
         const data = await readPokemons();       
-        const pokemonWidthID = data.filter(pokemon => {
+        const pokemonWidthID = data.find(pokemon => {
             const { id } = pokemon;
             if (id ===  parseInt(paramsID)) {
                 return pokemon;   
@@ -30,8 +30,27 @@ const getSinglePokemon = async (req, res) => {
 };
 
 const getSinglePokemonType = async (req, res) => {
-    
+    const { id, info} = req.params;
+
     try{
+        // console.log(typeof(info));
+        if((info === 'name') || (info === 'type') || (info === 'base')){
+            const data = await readPokemons();      
+            const pokemonWidthID = data.find(pokemon => {
+                //const { id } = pokemon;
+                if (pokemon.id ===  parseInt(id)) {
+                    return pokemon;   
+                }
+            });
+
+            if (pokemonWidthID.length === 0) {
+                res.send(`The id: ${id} does not exists!`);
+            }else{
+                res.json(pokemonWidthID[info]);
+            }
+        }else{
+            return res.send(`${info} is not a valid type. There is only name, type or base posible.`);   
+        }
         
     }catch(err){
         console.log(err);
